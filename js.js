4c459f38bb325fc146303cee03897e712fb1d534
Rishit Varshney <75286874+rishitv26@users.noreply.github.com>
2023-02-19 16:20:47 +0000

    dsdsd

--- js.js
+++ js.js
@@ -1,13 +1,15 @@
-const d = document.getElementById("distance")
-const kg = document.getElementById("weight")
-const r = document.getElementById("rounds")
 const out = document.getElementById("out")
 let cal
 
 function calculate() {
+    const d = document.getElementById("distance").value
+    const kg = document.getElementById("weight").value
+    const r = document.getElementById("rounds").value
+
     cal = 4/4184*(9.8*kg*d*r)
     out.innerHTML = `Total Calories Burnt: ${cal} Calories`
     alert(`Total Calories Burnt: ${cal} Calories`)
+    return false
 }
 
 function init() {
